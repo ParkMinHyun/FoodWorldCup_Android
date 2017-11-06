9d390b1971cfec5e3b0c0ddd4d5fb8c170b5cbd1
박민현 <algusl92@naver.com>
2017-11-07 01:42:06 +0900

    MenuWorldCup Activity 기본 UI 완성

--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
@@ -1,13 +1,19 @@
 package com.example.parkminhyun.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MenuWorldCupActivity extends AppCompatActivity {
 
+    ImageView topImage;
+    ImageView downImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_world_cup);
+
+        topImage = (ImageView)findViewById(R.id.topImage);
+//        topImage.setAlpha(0.3f);
     }
 }
