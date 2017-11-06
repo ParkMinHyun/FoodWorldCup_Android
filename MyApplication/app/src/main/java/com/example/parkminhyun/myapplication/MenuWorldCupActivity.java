1dcc6ab7e2eae9b8b61f7dc1eb6d579e2cd9e6cf
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-11-06 23:35:32 +0900

    FoodWolrdCup 초기 환경 설정 완료

---
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
@@ -0,0 +1,13 @@
+package com.example.parkminhyun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuWorldCupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_world_cup);
+    }
+}
