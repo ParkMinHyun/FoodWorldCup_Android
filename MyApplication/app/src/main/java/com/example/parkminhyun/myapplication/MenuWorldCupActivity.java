ed7cf724791b94b311090af194811e7cf4529271
박민현 <algusl92@naver.com>
2017-11-07 12:52:11 +0900

    음식 이미지 추가 및 StringBuilder에 음식 Name을 저장해 음식 교체 기능 완성

--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
@@ -8,12 +8,33 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MenuWorldCupActivity extends AppCompatActivity {
 
     View view;
     FrameLayout roots, menuLayout;
     ImageView topImage, downImage;
+
+
+    private StringBuilder[] foodNameStringBuilder = {
+            new StringBuilder("bibimbab"),
+            new StringBuilder("cake"),
+            new StringBuilder("chicken"),
+            new StringBuilder("dduckppoki"),
+            new StringBuilder("donggas"),
+            new StringBuilder("gimbab"),
+            new StringBuilder("hamburger"),
+            new StringBuilder("ramen"),
+            new StringBuilder("spagetti"),
+            new StringBuilder("steak"),
+            new StringBuilder("sushi"),
+            new StringBuilder("zazang")};
+
+    private String mDrawableName1, mDrawableName2;
+    private int resID1, resID2;
+    private int foodIndex = 0;
+    int a = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,21 @@ public class MenuWorldCupActivity extends AppCompatActivity {
 
         view = findViewById(R.id.includeFrame);
         findViewInit();
+        foodImageViewSetting();
+    }
+
+    public void foodImageViewSetting() {
+        // 문자열로 이미지 가져오기
+        mDrawableName1 = foodNameStringBuilder[foodIndex++].toString();
+        resID1 = getResources().getIdentifier(mDrawableName1 , "drawable", getPackageName());
+        topImage.setImageResource(resID1);
+
+        mDrawableName2 = foodNameStringBuilder[foodIndex++].toString();
+        resID2 = getResources().getIdentifier(mDrawableName2 , "drawable", getPackageName());
+        downImage.setImageResource(resID2);
+
+        topImage.setAlpha(1f);
+        downImage.setAlpha(1f);
     }
 
     public void topImageClick(View v) throws InterruptedException {
@@ -29,19 +65,19 @@ public class MenuWorldCupActivity extends AppCompatActivity {
 
         // 1초 뒤 Handler 실행
         Handler myHandler = new Handler();
-        myHandler.postDelayed(mMyRunnable, 1000);
+        myHandler.postDelayed(mMyRunnable1, 1000);
     }
 
     public void downImageClick(View v) {
         topImage.setAlpha(0.3f);
     }
 
-    private Runnable mMyRunnable = new Runnable() {
+    private Runnable mMyRunnable1 = new Runnable() {
         @Override
         public void run() {
             menuLayout.startAnimation(new Animate());
 
-            // 1초 뒤 Handler 실행
+            // 0.1초 뒤 Handler 실행
             Handler Handler = new Handler();
             Handler.postDelayed(mMyRunnable2, 100);
         }
@@ -52,7 +88,7 @@ public class MenuWorldCupActivity extends AppCompatActivity {
         public void run() {
             roots.removeView(view);
 
-            // 1초 뒤 Handler 실행
+            // 0.1초 뒤 Handler 실행
             Handler Handler = new Handler();
             Handler.postDelayed(mMyRunnable3, 100);
         }
@@ -61,12 +97,16 @@ public class MenuWorldCupActivity extends AppCompatActivity {
     private Runnable mMyRunnable3 = new Runnable() {
         @Override
         public void run() {
+
+            // view 다시 추가 한 뒤 ImageView 채우기
             roots.addView(view);
+            foodImageViewSetting();
+
+            // 서서히 나타나는 애니메이션 적용
             Animation fade_in_animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
             view.startAnimation(fade_in_animation);
 
-            topImage.setAlpha(1f);
-            downImage.setAlpha(1f);
+            Toast.makeText(getApplicationContext(), String.valueOf(a++),Toast.LENGTH_SHORT).show();
         }
     };
 
