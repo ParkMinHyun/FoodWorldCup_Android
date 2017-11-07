462593912d71f85b516c49af55b916d31ffb15be
박민현 <algusl92@naver.com>
2017-11-07 20:59:29 +0900

    결과창 뜨는 애니메이션 추가 및 음식 클릭했을 때 나타나는 check 이미지 추가

--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/MenuWorldCupActivity.java
@@ -1,6 +1,5 @@
 package com.example.parkminhyun.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +8,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +17,7 @@ public class MenuWorldCupActivity extends AppCompatActivity {
 
     View view;
     FrameLayout roots, menuLayout;
+    ImageView topImageCheck, downImageCheck;
     ImageView topImage, downImage;
 
     private List<String> foodTournerment_menuList = new ArrayList<String>();
@@ -25,47 +26,72 @@ public class MenuWorldCupActivity extends AppCompatActivity {
     private int foodIndex = 0;
     private boolean quarterfinal_flag = false, semifinal_flag = false, final_flag = false;
 
+    private Animation translateUpAnim;
+    private LinearLayout resultPage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_world_cup);
 
         view = findViewById(R.id.includeFrame);
-        findViewInit();
+
+        propertyInit();
         menuListInit();
         foodImageViewSetting();
     }
 
-    public void findViewInit() {
+    public void propertyInit() {
         roots = (FrameLayout) findViewById(R.id.root);
         menuLayout = (FrameLayout) findViewById(R.id.menuLayout);
         topImage = (ImageView) findViewById(R.id.topImage);
         downImage = (ImageView) findViewById(R.id.downImage);
+        resultPage = (LinearLayout) findViewById(R.id.result_page);// 슬라이딩으로보여질레이아웃객체참조
+
+        topImageCheck = (ImageView) findViewById(R.id.topImageCheck);
+        downImageCheck = (ImageView) findViewById(R.id.downImageCheck);
+
+
+        // 애니메이션객체로딩
+        translateUpAnim = AnimationUtils.loadAnimation(this, R.anim.translate_up);
+
+        // 애니메이션객체에리스너설정
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+        translateUpAnim.setAnimationListener(animListener);
+    }
+
+    // 애니메이션 리스너 정의
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationRepeat(Animation animation) {}
+        @Override
+        public void onAnimationStart(Animation animation) {}
+        // 애니메이션이 끝날때 호출되는 메소드
+        public void onAnimationEnd(Animation animation) {}
     }
 
     public void foodImageViewSetting() {
 
         // 16강, 8강, 4강 진행
-        if(foodIndex == 8 && quarterfinal_flag == false) {
+        if (foodIndex == 8 && quarterfinal_flag == false) {
             foodIndex = 0;
-            quarterfinal_flag =true;
-        }
-        else if(foodIndex == 4 && quarterfinal_flag == true){
+            quarterfinal_flag = true;
+        } else if (foodIndex == 4 && quarterfinal_flag == true) {
             foodIndex = 0;
             semifinal_flag = true;
-        }
-        else if(foodIndex == 2 && semifinal_flag == true){
+        } else if (foodIndex == 2 && semifinal_flag == true) {
             foodIndex = 0;
             final_flag = true;
         }
 
         // 문자열로 drawable에 있는 음식 이미지 가져오기
         mDrawableName1 = foodTournerment_menuList.get(foodIndex++).toString();
-        resID1 = getResources().getIdentifier(mDrawableName1 , "drawable", getPackageName());
+        resID1 = getResources().getIdentifier(mDrawableName1, "drawable", getPackageName());
         topImage.setImageResource(resID1);
 
         mDrawableName2 = foodTournerment_menuList.get(foodIndex++).toString();
-        resID2 = getResources().getIdentifier(mDrawableName2 , "drawable", getPackageName());
+        resID2 = getResources().getIdentifier(mDrawableName2, "drawable", getPackageName());
         downImage.setImageResource(resID2);
 
         // 이미지 클릭 활성화 및 투명도 초기화
@@ -73,7 +99,7 @@ public class MenuWorldCupActivity extends AppCompatActivity {
         downImage.setAlpha(1f);
     }
 
-    public void menuListInit(){
+    public void menuListInit() {
         foodTournerment_menuList.add("baekban");
         foodTournerment_menuList.add("bibimbab");
         foodTournerment_menuList.add("bread");
@@ -93,21 +119,23 @@ public class MenuWorldCupActivity extends AppCompatActivity {
     }
 
     // Food 토너먼트 중 음식 이미지를 클릭했을 경우
-    private void FoodImageClick_Result(String mode){
+    private void FoodImageClick_Result(String mode) {
 
         // Click Event 비활성화
         downImage.setClickable(false);
         topImage.setClickable(false);
 
         // 투명도 설정 + String 배열 삭제
-        if(mode.equals("topImageClick"))
-        {
+        if (mode.equals("topImageClick")) {
             downImage.setAlpha(0.3f);
-            foodTournerment_menuList.remove((foodIndex--) -1);
-        }
-        else{
+            topImageCheck.bringToFront();
+            topImageCheck.setVisibility(view.VISIBLE);
+            foodTournerment_menuList.remove((foodIndex--) - 1);
+        } else {
             topImage.setAlpha(0.3f);
-            foodTournerment_menuList.remove((foodIndex--)-2);
+            downImageCheck.bringToFront();
+            downImageCheck.setVisibility(view.VISIBLE);
+            foodTournerment_menuList.remove((foodIndex--) - 2);
         }
     }
 
@@ -122,7 +150,7 @@ public class MenuWorldCupActivity extends AppCompatActivity {
 
     // 아래 이미지 클릭시
     public void downImageClick(View v) {
-        FoodImageClick_Result("topImageClick");
+        FoodImageClick_Result("downImageClick");
 
         // 1초 뒤 Handler 실행
         Handler myHandler = new Handler();
@@ -137,13 +165,16 @@ public class MenuWorldCupActivity extends AppCompatActivity {
             // left Move animation 실행
             menuLayout.startAnimation(new Animate());
 
+            // check 이미지 Invisible
+            topImageCheck.setVisibility(view.INVISIBLE);
+            downImageCheck.setVisibility(view.INVISIBLE);
+
             // 결승전 진행할 때 결과창 Activity 띄우기
-            if(final_flag)
-            {
-                Intent intent = new Intent(getApplicationContext(),FoodWorldCupResultActivity.class);
-                startActivity(intent);
-                MenuWorldCupActivity.this.finish();
+            if (final_flag) {
+                resultPage.setVisibility(view.VISIBLE);
+                resultPage.startAnimation(translateUpAnim);
             }
+
             // 결승전 아닐 경우 Handler2 실행
             else {
                 // 0.1초 뒤 Handler 실행
@@ -178,9 +209,9 @@ public class MenuWorldCupActivity extends AppCompatActivity {
             Animation fade_in_animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
             view.startAnimation(fade_in_animation);
 
+            // topImage, downImage 다시 클릭 이벤트 활성화
             downImage.setClickable(true);
             topImage.setClickable(true);
-
         }
     };
 }
