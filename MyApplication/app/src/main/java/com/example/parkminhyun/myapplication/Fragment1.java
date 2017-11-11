dfcd6fb8341913e7ac1891724fe04c358fe496e7
박민현 <algusl92@naver.com>
2017-11-12 01:36:38 +0900

    FragmentMapActivity Search bar 디자인 적용

--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/Fragment1.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/Fragment1.java
@@ -12,16 +12,19 @@ import com.nhn.android.maps.NMapView;
 public class Fragment1 extends Fragment {
     private NMapContext mMapContext;
     private static final String CLIENT_ID = "_i9KY4aNfTTMFa386pMB";// 애플리케이션 클라이언트 아이디 값
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment1, container, false);
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMapContext =  new NMapContext(super.getActivity());
         mMapContext.onCreate();
     }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/FragmentMapActivity.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/FragmentMapActivity.java
@@ -5,18 +5,42 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FragmentMapActivity extends FragmentActivity {
+    Button deleteBtn;
+    EditText inputLocation;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
 
+        propertyInit();
+        showNaverMapView();
+
+    }
+
+    // id 추가 및 기타 속성 초기화
+    public void propertyInit() {
+        inputLocation = (EditText)findViewById(R.id.search);
+        inputLocation.setCompoundDrawables(null, null, getResources().getDrawable(R.drawable.close), null);
+
+        deleteBtn = (Button)findViewById(R.id.deleteBtn);
+    }
+
+    public void showNaverMapView(){
         Fragment1 fragment1 = new Fragment1();
         fragment1.setArguments(new Bundle());
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.fragmentHere, fragment1);
         fragmentTransaction.commit();
+    }
+
+    public void deleteBtnClicked(View v){
+        inputLocation.setText("");
     }
 }
