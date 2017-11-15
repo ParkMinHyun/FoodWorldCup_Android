4bc73bed98d4a2926a14577d2d5e2f319de6f8a6
박민현 <algusl92@naver.com>
2017-11-15 14:32:56 +0900

    구글맵 연동 및 현재 위치 띄우기

--- MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/ResultFoodMapActivity.java
+++ MyApplication/app/src/main/java/com/example/parkminhyun/myapplication/ResultFoodMapActivity.java
@@ -1,25 +1,77 @@
 package com.example.parkminhyun.myapplication;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
-import com.nhn.android.maps.NMapActivity;
-import com.nhn.android.maps.NMapView;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ResultFoodMapActivity extends NMapActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    private NMapView mMapView;// 지도 화면 View
-    private final String CLIENT_ID = "_i9KY4aNfTTMFa386pMB";// 애플리케이션 클라이언트 아이디 값
+public class ResultFoodMapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GPSInfo gpsInfo;
+
+    private GoogleMap gMap;
+    private Geocoder gCorder;
+
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMapView = new NMapView(this);
-        setContentView(mMapView);
-        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
-        mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.requestFocus();
+        setContentView(R.layout.activity_result_food_map);
+
+        gpsInfo = new GPSInfo(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        LatLng currentPos = new LatLng(gpsInfo.getLatitude(),gpsInfo.getLongitude());
+        gMap.addMarker(new MarkerOptions().position(currentPos).title("Marker in Sydney"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(currentPos));
+//        Geocoder gCoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+//        List<Address> addr = null;
+//        try {
+//            addr = gCoder.getFromLocation(latitude, longitude, 1);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Address a = addr.get(0);
+
+//        a.getAdminArea()+" "+a.getLocality()+" "+a.getThoroughfare();
+
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
